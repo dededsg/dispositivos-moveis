f2892a5fb8383cb2282dcf3beb252481df8c6eff
Develin <develinsouzag@gmail.com>
2023-04-10 16:53:24 -0300

    versão 3.0

---
+++ aula6/salario.js
@@ -0,0 +1,8 @@
+let salario = 1000
+
+while (salario < 5000){
+    console.log(`Greve aaaaaaaaaaaaaaaaaa só R$: ${salario}`);
+    
+    salario += 100;
+}
+console.log(`É pouco mas aceito,  R$: ${salario}`);
---
+++ aula6/salario2.js
@@ -0,0 +1,10 @@
+let salario = 1000
+
+do {
+    console.log(`Greve aaaaaaaaaaaaaaaaaa só R$: ${salario}`);
+    
+    salario += 100;
+}
+while (salario <= 5000)
+
+console.log(`UFA é pouco mas é melhor ${salario}`);
